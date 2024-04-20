cee7fb1ad02b23a94b7b58daf452f4274e48d43e
Rahimi <u2101345@siswa.um.edu.my>
2024-04-21 00:43:59 +0800

    Completed tutorial 12

--- scripts/11-todo-list.js
+++ scripts/11-todo-list.js
@@ -1,4 +1,7 @@
-const todoList = ['make dinner','wash dishes'];
+const todoList = [{
+    name : 'masak',
+    dueDate : '2024-11-02'
+}];
 
 renderTodoList();
 
@@ -8,9 +11,21 @@ function renderTodoList(){
     let todoListHTML = '';
 
     for(let i = 0 ; i < todoList.length ; i++){
-        const todo = todoList[i];
+        const todoObject = todoList[i];
+        //const name = todoObject.name;
+        //const  dueDate = todoObject.dueDate;
+        //Destructuring
+        const{name,dueDate} = todoObject;
         //genereting html in js
-        const html = `<p> ${todo} </p>`;
+        //Create delete button html in js
+        const html = `
+        <div>${name}</div>
+        <div>${dueDate}</div>
+        <button onclick="
+            todoList.splice(${i},1);
+            renderTodoList();
+        " class="delete-button"> Delete</button>
+        `;
         todoListHTML += html;
     }
 
@@ -23,7 +38,15 @@ function addTodo(){
 
     const name = inputElement.value;
 
-    todoList.push(name);
+    const dateInput = document.querySelector('.js-date-input');
+    const dueDate = dateInput.value;
+
+    //push task inserted
+    todoList.push({
+        name: name ,
+        dueDate : dueDate
+    });
+
     console.log(todoList);
 
     inputElement.value = '';
